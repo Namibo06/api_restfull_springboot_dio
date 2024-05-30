7f25c8b2ee450dc125420dac4d9ce59249e2b927
Namibo06 <namibo654@gmail.com>
2024-05-30 19:23:54 -0300

    add models

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_feature")
+public class Feature extends BaseItem{
+
+}
---
+++ src/main/java/me/dio/domain/model/News.java
@@ -0,0 +1,9 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_news")
+public class News extends BaseItem{
+
+}
